e62538d102ff331732fa7223ec27d49a2e693ac7
AndroidBirdBoom <1214447320@qq.com>
2016-07-21 18:57:14 +0800

    second change

--- NoteBooks/app/src/main/java/com/example/jack/notebooks/NotesDB.java
+++ NoteBooks/app/src/main/java/com/example/jack/notebooks/NotesDB.java
@@ -1,8 +1,13 @@
+/**
+ *  Author :Wang
+ *  功能：数据库管理类的构建
+ */
 package com.example.jack.notebooks;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 /**
  * Created by Jack on 2016/7/20.
@@ -14,9 +19,15 @@ public class NotesDB extends SQLiteOpenHelper {
     public static final String ID = "_id";
     public static final String TIME = "time";
 
-    public NotesDB(Context context) {
-        super(context, "notes", null, 1);
+    public static final String TABLE_NAME_LOGIN = "lagin";
+    public static final String IID = "_iid";
+    public static final String ACCOUNT = "account";
+    public static final String TABLE_MIMA = "password";
 
+    private Context mContext;
+    public NotesDB(Context context) {
+        super(context, "notes.db", null, 2);
+        mContext = context;
     }
 
     @Override
@@ -24,10 +35,17 @@ public class NotesDB extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("CREATE TABLE "+ TABLE_NAME + "(" + ID
                 + " INTEGER PRIMARY KEY AUTOINCREMENT, " + CONTENT
                 + " TEXT NOT NULL," + TIME +" TEXT NOT NULL)");
+
+        sqLiteDatabase.execSQL("CREATE TABLE "+ TABLE_NAME_LOGIN + "(" + IID
+                + " INTEGER PRIMARY KEY AUTOINCREMENT, " + ACCOUNT
+                + " TEXT NOT NULL," + TABLE_MIMA + " TEXT NOT NULL)");
+        Toast.makeText(mContext,"success", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL("drop table if exists" + TABLE_NAME);
+        sqLiteDatabase.execSQL("drop table if exists" + TABLE_NAME_LOGIN);
+        onCreate(sqLiteDatabase);
     }
 }
